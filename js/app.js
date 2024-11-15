7698daef96162660bdd0fce53579d589d8d019e5
Christina Sarai Atoche Pascual <christinas.apascual@gmail.com>
2024-11-15 18:31:53 -0500

    feat: Agrega paginación

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 // -------------------------- SELECTORES --------------------------
 const form = document.querySelector('#form');
 const result = document.querySelector('#result');
+const pagination = document.querySelector('#pagination');
+
+// -------------------------- VARIABLES --------------------------
+const n = 30;
+let totalPage;
+let iterador;
 
 // -------------------------- EVENTOS --------------------------
 
@@ -16,6 +22,12 @@ function validate(event) {
     const terminoInput = document.querySelector("#termino").value;
 
     if(terminoInput.trim() === '') {
+        clearHtml(result);
+
+        if(result.classList.contains('grid')) {
+            result.classList.remove('grid');
+        }
+
         showAlert('Error: Agregue un término de búsqueda.');
         return;
     }
@@ -28,22 +40,30 @@ function validate(event) {
 // consulta api
 function searchImages(termino) {
     const key = "47080193-968844eeb3e2617f32c8d5b3f";
-    const apiURL = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const apiURL = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${n}`;
 
     fetch(apiURL)
         .then(result => result.json())
-        .then(result => showImages(result.hits))
+        .then(result => {
+            totalPage = calculate(result.totalHits)
+            showImages(result.hits)
+        })
 }
 
 
 // muestra imagenes
 function showImages(images) {
-    
-    // borra resultados previos
-    while(result.firstChild) {
-        result.removeChild(result.firstChild);
+    clearHtml(result)
+
+    if(images.length === 0) {
+        if(result.classList.contains('grid')) {
+            result.classList.remove('grid')
+        }
+
+        showAlert('Lo siento, no encontramos resultados');
+        return;
     }
-    
+
     // itera resultados
     images.forEach(img => {
         const {downloads, largeImageURL, likes, previewURL, tags} = img;
@@ -62,7 +82,41 @@ function showImages(images) {
         result.classList.add('grid');
         result.appendChild(imgContainer);
     })
+
+    showPages()
     
+}
+
+
+// muestra paginadores
+function showPages() {
+    iterador = createGenerator(totalPage)
+
+    while(true) {
+        const {value, done} = iterador.next();
+        
+        if(done) return;
+
+        const page = document.createElement('A');
+        page.href = "#";
+        page.textContent = value;
+        page.dataset.pagima = value;
+        page.classList.add('page');
+
+        pagination.appendChild(page);
+    }
+}
+
+// calcular paginación
+function calculate(total) {
+    return parseInt(Math.ceil(total / n));
+}
+
+// generador de páginas
+function *createGenerator(total) {
+    for(let i = 1; i <= total; i++){
+        yield i;
+    }
 }
 
 
@@ -79,4 +133,11 @@ function showAlert(msg) {
     setTimeout(() => {
         alert.remove();
     }, 3000)
+}
+
+// borra resultados previos
+function clearHtml(container) {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
 }
